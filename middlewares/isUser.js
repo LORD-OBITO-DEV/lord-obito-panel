dde1fc77254f30dab572f3d1c8f62021d85f847c
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-11 22:31:05 +0000

    Update isUser.js

--- middlewares/isUser.js
+++ middlewares/isUser.js
@@ -1,1 +1,8 @@
+function isUser(req, res, next) {
+  if (req.session && req.session.user && !req.session.user.isAdmin) {
+    return next();
+  }
+  res.status(403).send('Accès refusé. Utilisateur uniquement.');
+}
 
+module.exports = { isUser };
