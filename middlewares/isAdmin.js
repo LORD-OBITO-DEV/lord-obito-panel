d1d08eedbeb202b49e045e227b2b21c11e610b22
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-11 22:29:54 +0000

    Update isAdmin.js

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,1 +1,8 @@
+function isAdmin(req, res, next) {
+  if (req.session && req.session.user && req.session.user.isAdmin) {
+    return next();
+  }
+  res.status(403).send('Accès refusé. Admin uniquement.');
+}
 
+module.exports = { isAdmin };
