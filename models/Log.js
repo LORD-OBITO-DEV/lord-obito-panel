d4a4a31a8566ce06dde86be58c376898e818753e
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-11 22:33:17 +0000

    Update Log.js

--- models/Log.js
+++ models/Log.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const LogSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  action: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+const logSchema = new mongoose.Schema({
+  username: { type: String, required: true }, // utilisateur lié au log
+  action: { type: String, required: true }, // description de l'action
+  timestamp: { type: Date, default: Date.now },
+  ip: { type: String }, // optionnel : adresse IP de l'action
 });
 
-module.exports = mongoose.model('Log', LogSchema);
+module.exports = mongoose.model('Log', logSchema);
