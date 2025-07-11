0fb01cf6db4d4123a2bf8c04fd7af9cdfe01d0b0
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-11 22:36:28 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connecté avec succès');
+  } catch (error) {
+    console.error('❌ Erreur de connexion à MongoDB :', error.message);
+    process.exit(1); // Arrête l'app si la connexion échoue
+  }
+};
+
+module.exports = connectDB;
